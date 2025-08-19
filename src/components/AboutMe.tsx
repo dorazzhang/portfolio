20445a77f4d6af81d48263709d4a016fcc2c57bd
Dora Zhang <dorajzhang@gmail.com>
2025-08-19 01:12:03 -0700

    ACTUALLY LAST ONE

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -901,7 +901,7 @@ const AboutMe: React.FC = () => {
                 transition={{ delay: 0.6, duration: 0.6 }}
                 whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
               >
-                <HobbyImage src="/images/nexus.JPG"/>
+                <HobbyImage src="./images/nexus.JPG"/>
               </HobbyImageCard>
               <HobbyImageCard
                 initial={{ opacity: 0, scale: 0.8 }}
